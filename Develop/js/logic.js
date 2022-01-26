dc36c576691ca125022bb78183c7332d3702d2bb
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-01-26 15:44:57 +1100

    changed html and css for the header - added an enquirte button to open a form

--- Develop/js/logic.js
+++ Develop/js/logic.js
@@ -1,17 +1,3 @@
-const menuButton = document.getElementById("menu-button");
-
-// add event listener to menu button to show menu options
-menuButton.addEventListener("click", () => {
-    let menuContainer = document.getElementById("header-menu-container");
-    if (menuContainer.style.display == "none") {
-        menuContainer.style.display = "block";
-    }
-    else {
-        menuContainer.style.display = "none";
-    }
-});
-
-
 // Add event listener to the project buttons
 const projectButtons = document.getElementById("projects-list");
 projectButtons.addEventListener("click", (event) => {
